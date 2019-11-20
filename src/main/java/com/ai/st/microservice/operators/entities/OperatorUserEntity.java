a3ceecf654d105e4a537644122e91a6948af746a
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-20 13:40:19 -0500

    creation entities

---
+++ src/main/java/com/ai/st/microservice/operators/entities/OperatorUserEntity.java
@@ -0,0 +1,73 @@
+package com.ai.st.microservice.operators.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "operators_users", schema = "operators")
+public class OperatorUserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "user_code", nullable = false)
+	private Long userCode;
+
+	@Column(name = "created_at", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "operator_id", referencedColumnName = "id", nullable = false)
+	private OperatorEntity operator;
+
+	public OperatorUserEntity() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserCode() {
+		return userCode;
+	}
+
+	public void setUserCode(Long userCode) {
+		this.userCode = userCode;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public OperatorEntity getOperator() {
+		return operator;
+	}
+
+	public void setOperator(OperatorEntity operator) {
+		this.operator = operator;
+	}
+
+}
