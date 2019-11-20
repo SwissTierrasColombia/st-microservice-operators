170af3ff95ca8ca0fbeb87149e28c90d129a386c
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-20 13:27:43 -0500

    configuration project

---
+++ src/main/java/com/ai/st/microservice/operators/StMicroserviceOperatorsApplication.java
@@ -0,0 +1,15 @@
+package com.ai.st.microservice.operators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class StMicroserviceOperatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StMicroserviceOperatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/st/microservice/operators/StMicroserviceOperatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.operators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMicroserviceOperatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
